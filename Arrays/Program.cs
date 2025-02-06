3c38f041c6badeb7a8cbd8c8396995bff388fb18
Del1-SUDO <kurbonovs397@gmail.com>
2025-02-06 15:55:37 +0500

    arrays G

---
+++ Arrays/Program.cs
@@ -0,0 +1,127 @@
+﻿class Program
+{
+    static void Main (string[] args)
+    {
+
+        /* Task A */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     if (i%2 == 0)
+        //     {
+        //         System.Console.Write(numbers[i] + " ");
+        //     }
+        // }
+
+
+        /* Task B */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+
+        // foreach (var item in numbers)
+        // {
+        //     if (item%2 == 0)
+        //     {
+        //         System.Console.Write(item + " ");
+        //     }
+        // }
+
+
+        /* Task C */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+        // int cnt = 0;
+        // foreach (var item in numbers)
+        // {
+        //     if (item >= 0)
+        //     {
+        //         cnt++;
+        //     }
+        // }
+        // System.Console.WriteLine(cnt);
+
+
+        /* Task D */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+        // int cnt = 0;
+        // for (int i = 0; i < numbers.Length-1; i++)
+        // {
+        //     if (numbers[i] < numbers[i+1])
+        //     {
+        //         cnt++;
+        //     }
+        // }
+        // System.Console.WriteLine(cnt);
+
+
+        /* Task E */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+        // int cnt = 0;
+        // for (int i = 0; i < numbers.Length-1; i++)
+        // {
+        //     if (numbers[i] > 0 && numbers[i+1] > 0 || numbers[i] < 0 && numbers[i+1] < 0)
+        //     {
+        //         cnt++;
+        //     }
+        // }
+        // if (cnt > 0)
+        // {
+        //     System.Console.WriteLine("YES");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine("NO");
+        // }
+
+
+        /* Task F */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+        // int cnt = 0;
+        // for (int i = 1; i < numbers.Length-1; i++)
+        // {
+        //     if (numbers[i-1] < numbers[i] && numbers[i] > numbers[i+1])
+        //     {
+        //         cnt++;
+        //     }
+        // }
+        // System.Console.WriteLine(cnt);
+
+
+        /* Task G */
+        // System.Console.WriteLine("Enter numbers in a row with space between them: ");
+        // string input = Console.ReadLine();
+        // int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+
+        // int start = 0;
+        // int end = numbers.Length - 1;
+        
+        // while (start < end)
+        // {
+        //     int temp = numbers[start];
+        //     numbers[start] = numbers[end];
+        //     numbers[end] = temp;
+
+        //     start++;
+        //     end--;
+        // }
+
+        // foreach (int num in numbers)
+        // {
+        //     Console.Write(num + " ");
+        // }
+
+
+
+    }
+}
